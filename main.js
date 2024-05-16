aa769f0911e2b9eeada1a0bdd2d91d217a01eb07
Mehaknaz <159727990+Mehaknz@users.noreply.github.com>
2024-05-16 06:44:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+import inquirer from 'inquirer';
+let todos = []; //shopper[] 
+let condition = true;
+while (condition) {
+    let todoquestions = await inquirer.prompt([
+        {
+            name: "firstquestion",
+            type: "input",
+            message: "what would you like to add in your todos?"
+        },
+        {
+            name: "secondquestion",
+            type: "confirm",
+            message: "would you like to add more in your todos?",
+            default: "true" //take value yes because i use true
+        }
+    ]);
+    todos.push(todoquestions.firstquestion);
+    console.log(todos);
+    // loop condition
+    condition = todoquestions.secondquestion;
+}
